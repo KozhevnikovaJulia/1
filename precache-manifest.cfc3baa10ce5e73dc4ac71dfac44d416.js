671ba2e2da0106a2be8b153acb2afc7c174296ad
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2020-10-19 09:25:49 +0300

    Updates

---
+++ precache-manifest.cfc3baa10ce5e73dc4ac71dfac44d416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277e6584aa02db503a263f71ba395041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "491f9665a7a5f8789c28",
+    "url": "/static/css/main.15f15777.chunk.css"
+  },
+  {
+    "revision": "4ccb794f5dc0fe74badb",
+    "url": "/static/js/2.789118b7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.789118b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "491f9665a7a5f8789c28",
+    "url": "/static/js/main.9494628d.chunk.js"
+  },
+  {
+    "revision": "b8c053cb2522392dd525",
+    "url": "/static/js/runtime-main.d9469fe2.js"
+  }
+]);
